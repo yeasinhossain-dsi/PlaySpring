f0060ea80cadda18823287fe7ed7734b0dbe573f
Yeasin Hossain <yeasinhossain.dsi@gmail.com>
2020-01-30 18:20:55 +0600

    Added Multiple routes & templates

--- src/main/java/com/playspring/backend/HomeController.java
+++ src/main/java/com/playspring/backend/HomeController.java
@@ -1,25 +1,55 @@
 package com.playspring.backend;
 
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+
+
+@Controller
 public class HomeController {
 
     @GetMapping("/")
+    @ResponseBody
     public String getMethodName() {
         return "What Up nigga";
     }
 
     @GetMapping("/getParams")
+    @ResponseBody
     public String getMethodName(CParms param) {
         return "Get Params " + param.getA() + " " + param.getB();
     }
 
+    @GetMapping("/namedParams/{a}")
+    @ResponseBody
+    public String namedVariable(@PathVariable("a") String a) {
+        return "Get Params " + a;
+    }
+
     @PostMapping("/postParams")
+    @ResponseBody
     public String postMethodName(CParms param) {
         return "Post Params " + param.getA() + " " + param.getB();
+    }
+
+    @GetMapping("/json")
+    @ResponseBody
+    public Map<String, String> heyJson(){
+        Map<String, String> response = new HashMap<>();
+        response.put("name", "John Doe");
+        return response;
+    }
+
+    @GetMapping("/theme")
+    public String theme(Model model) {
+        model.addAttribute("name", "Mokbul");
+        return "welcome";
     }
 
 }
