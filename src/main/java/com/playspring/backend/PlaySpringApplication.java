19f6d0866998e62e07b207d077bc3a6cda7e36ad
Yeasin Hossain <yeasinhossain.dsi@gmail.com>
2020-01-30 18:28:59 +0600

    Renamed main Application

--- src/main/java/com/playspring/backend/PlaySpringApplication.java
+++ src/main/java/com/playspring/backend/PlaySpringApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class PlaySpringApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(PlaySpringApplication.class, args);
 	}
 
 }
